9d9c9bb4f01b0efe839c49831e56fc67930f6418
morgan.d.carson <morgan.d.carson@gmail.com>
2025-06-07 04:49:54 +0000

    added ti_84

---
+++ ti_84.py
@@ -0,0 +1,3 @@
+# This file is intended for a TI-84 calculator simulation.
+# Building a full TI-84 simulation is a complex task involving graphics, input handling,
+# and implementing numerous mathematical and programming functions.
